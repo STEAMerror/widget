fd6b8ad4f5c0d4f59cacd9a343419326a50b2d30
STEAMerror <55356151+STEAMerror@users.noreply.github.com>
2020-07-06 10:24:21 +0300

    optimization

--- php/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = '127.0.0.1'; //адрес сервера 
-$database = 'weather'; //имя базы данных
-$user = 'root'; //имя пользователя
-$password = ''; //пароль
-
-$con = mysqli_connect($host, $user, $password, $database) //получаем ссылку подключения
-or die("Ошибка: " . mysqli_error($con));
-?>
